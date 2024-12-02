d9b315b76c4e34fbb9349ff1fdc080f5f08a993d
komalshrivas <shrivas.komal1990@gmail.com>
2024-12-02 21:26:36 +0530

    New changes

--- tests/dummyfile.spec.js
+++ tests/dummyfile.spec.js
@@ -7,3 +7,5 @@
 //npx playwright test ClientAppPOJ.spec.js --config playwright.config1.js --project firefox = select browser
 //npx playwright test ClientAppPOJ.spec.js --config playwright.config1.js --project "Mobile Safari"
 // npm run Regressio
+// "WebTests": "npx playwright test --grep @Web",
+//     "API Test": "npz playwright test --grep @API"
