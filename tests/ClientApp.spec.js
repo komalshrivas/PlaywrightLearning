a6f807f98763a7dd3a717dd1d97a56769cea544a
komalshrivas <shrivas.komal1990@gmail.com>
2024-12-26 16:19:18 +0530

    New Code

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,19 +1,19 @@
 const { test, expect } = require('@playwright/test');
-test('@Web Client App login', async ({ page }) => {
-    //js file- Login js, DashboardPage
-    const email = "komalshrivas@yopmail.com"; // Define globel
-   // const productName = 'zara coat 3'; // Define globel
-   // const products = page.locator(".card-body"); // Define globel
-    await page.goto("https://rahulshettyacademy.com/client");
-    await page.locator("#userEmail").fill(email);
-    await page.locator("#userPassword").fill("Password@1");
-    await page.locator("[value='Login']").click();
-    await page.waitForLoadState('networkidle');
-    await page.locator(".card-body b").first().waitFor();
-    const titles = await page.locator(".card-body b").allTextContents();
-    console.log(titles); 
+// test('@Web Client App login', async ({ page }) => {
+//     //js file- Login js, DashboardPage
+//     const email = "komalshrivas@yopmail.com"; // Define globel
+//    // const productName = 'zara coat 3'; // Define globel
+//    // const products = page.locator(".card-body"); // Define globel
+//     await page.goto("https://rahulshettyacademy.com/client");
+//     await page.locator("#userEmail").fill(email);
+//     await page.locator("#userPassword").fill("Password@1");
+//     await page.locator("[value='Login']").click();
+//     await page.waitForLoadState('networkidle');
+//     await page.locator(".card-body b").first().waitFor();
+//     const titles = await page.locator(".card-body b").allTextContents();
+//     console.log(titles); 
   
- })
+//  })
 test('@B2B login', async ({ page }) => {
     //js file- Login js, DashboardPage
     await page.goto('https://b2b-platform.qa.trajector-tech.com/login');
